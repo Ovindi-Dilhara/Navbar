7f0342f6df11a8afe046c7053cd79562a4452d2b
Ovindi-Dilhara <148680250+Ovindi-Dilhara@users.noreply.github.com>
2024-06-23 13:11:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'flex' ? 'none' : 'flex';
+}
